f287be78dcdecf097b6a03724e20cba527c096e3
alan_sealy <alan.sealy@gmail.com>
2021-02-15 19:10:25 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import List from './components/List';
+import Edit from './components/Edit';
+import Create from './components/Create';
+import Show from './components/Show';
 
 ReactDOM.render(
+  <Router>
+      <div>
+          <Route render ={()=> < App />} path="/" />
+          <Route render ={()=> < List />} path="/list" />
+          <Route render ={()=> < Edit />} path="/edit/:id" />
+          <Route render ={()=> < Create />} path="/create" />
+          <Route render ={()=> < Show />} path="/show/:id" />
+      </div>
+  </Router>, document.getElementById('root'));
+
+
+/*ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+);*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
